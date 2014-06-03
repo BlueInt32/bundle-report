ffe94b9d3ce9a951e21d088678801467baa684e5
BlueInt32 <simon.budin@gmail.com>
2014-06-03 09:13:27 +0200

    hihi

--- Collecte.BackOffice/js/app.js
+++
@@ -1,6 +0,0 @@
-﻿//'use strict';
-
-//angular.module('myServices', []);
-angular.module('myControllers', []);
-//angular.module('myDirectives', []);
-angular.module('app', ['myServices', 'myControllers', 'myDirectives']);
